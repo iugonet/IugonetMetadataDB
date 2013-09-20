55fb023975d61f1695b183c02796fa69cc417915
iugonet <iugonet@h153.89.226.10.20871.vlan.kuins.net>
2013-09-20 19:16:27 +0900

    Fixed

---
+++ etc/profile.d/ant.sh
@@ -0,0 +1,1 @@
+export ANT_OPTS="-Dhttp.proxyHost=proxy.kuins.net -Dhttp.proxyPort=8080"
---
+++ etc/profile.d/proxy.sh
@@ -0,0 +1,4 @@
+export http_proxy=http://proxy.kuins.net:8080/
+export ftp_proxy=http://proxy.kuins.net:8080/
+export HTTP_PROXY=http://proxy.kuins.net:8080/
+export FTP_PROXY=http://proxy.kuins.net:8080/
